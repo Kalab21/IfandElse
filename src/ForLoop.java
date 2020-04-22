be788e8e75dee54d0f66dddd9ad663e0f8ebb4dd
Kalab21 <kkebed21@gmail.com>
2020-04-22 11:29:17 -0400

    First commit

---
+++ src/ForLoop.java
@@ -0,0 +1,9 @@
+public class ForLoop {
+
+        public static void main(String[] args){
+            for(int i=6; i<24; i++){
+                System.out.println("Count is: " + i);
+            }
+        }
+    }
+
---
+++ src/IFandElse.java
@@ -0,0 +1,15 @@
+public class IFandElse {
+    public static void main(String[] args)
+    {
+        int a=8;
+        int b=2;
+        int c=5;
+        if(c>=a) {
+            System.out.println("goal");
+        }else {
+            System.out.println("it is not goal");
+        }
+
+    }
+}
+
